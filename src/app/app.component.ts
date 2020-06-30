3fe50879b544b699bad1cad901cba6682d1324c3
paulajt <paulajimte1997@gmail.com>
2020-06-30 19:52:06 +0100

    Mostrar productos y buscador.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import axios from "axios";
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { ProductApiService } from './product-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,29 @@ import axios from "axios";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tiendaonline';
+  faSearch = faSearch;
+  busqueda: string = "";
+  products: any = [];
+  show: any = [];
+  searching: boolean = false;
+
+  constructor ( private ProductService: ProductApiService){
+    this.getProducts();
+  }
+
+
+  async getProducts (){
+    this.products = await this.ProductService.getApiProducts();
+    this.searching = false;
+  }
+
+  searchItem (){
+    this.show = [];
+    this.searching = true;
+    this.products.forEach(product => {
+      if (product.title.toLowerCase().includes(this.busqueda.toLocaleLowerCase()) === true){
+        this.show.push(product);
+      }
+    });
+  }
 }
