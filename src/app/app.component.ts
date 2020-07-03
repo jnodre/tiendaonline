7af0953b427597c0ade299131ac98883b50d675e
khushbu10 <khushbu.lulla@gmail.com>
2020-07-03 09:52:13 +0100

    categorias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,15 +15,38 @@ export class AppComponent {
   products: any = [];
   show: any = [];
   searching: boolean = false;
+  productsCategory: any []; 
+  category : boolean = false;
+  categorias : string[] =["Todo"];
+  selectedCategory: string = "";
 
   constructor ( private ProductService: ProductApiService, public router: Router){
     this.getProducts();
+    this.getCategory();
   }
-
-
+  
   async getProducts (){
     this.products = await this.ProductService.getApiProducts();
     this.searching = false;
+    this.category = false;
+  }
+
+  getCategory(){
+    this.products.forEach(product => {
+      if(this.categorias.includes(product.category) == false)
+        this.categorias.push(product.category)
+      })
+      console.log(this.categorias)
+  }
+  getProductByCategory(){
+    this.show =[];
+    this.productsCategory =[];
+    this.category = true;
+    if(this.selectedCategory == "Todo"){
+      this.productsCategory= this.products;
+    }else{
+      this.products.filter(product => product.category == this.selectedCategory).forEach(item => this.productsCategory.push(item))
+    }
   }
 
   searchItem (){
@@ -36,6 +59,4 @@ export class AppComponent {
     });
     this.busqueda = "";
   }
-
-  
 }
